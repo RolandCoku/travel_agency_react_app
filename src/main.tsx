9cf6cc425927a8278e18ec3c15f30abff54d1e97
RolandCoku <roland.coku@fti.edu.al>
2025-04-06 14:35:02 +0200

    Changed the imports of styles to let vite handle them

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-
+import './assets/styles/styles.css'    // ← Vite will bundle this
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
